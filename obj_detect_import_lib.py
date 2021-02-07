3542dc58abd41972387ab635452a4b5ddb679c9d
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-07 16:07:46 +0530

    add py

--- obj_detect_import_lib.py
+++
@@ -1,6 +0,0 @@
-import os
-import re
-import cv2
-import numpy as np
-from os.path import isfile, join
-import matplotlib.pyplot as plt
--- obj_detect_read_frames.py
+++
@@ -1,4 +0,0 @@
-# get the names of the files from the frames folder
-col_frames = os.listdir('frames/')
-
-# sort the files accordingly
