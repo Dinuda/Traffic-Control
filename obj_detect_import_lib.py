b0968d39f8f0243f8d9cf0bf2b1f8f1bd82469a2
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-07 15:50:41 +0530

    initial commit

---
+++ obj_detect_import_lib.py
@@ -0,0 +1,6 @@
+import os
+import re
+import cv2
+import numpy as np
+from os.path import isfile, join
+import matplotlib.pyplot as plt
---
+++ obj_detect_read_frames.py
@@ -0,0 +1,4 @@
+# get the names of the files from the frames folder
+col_frames = os.listdir('frames/')
+
+# sort the files accordingly
